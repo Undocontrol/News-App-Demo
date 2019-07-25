b569473280c778b848974d29216708ed86320f71
Emma P <undocontrol88@gmail.com>
2019-07-25 19:45:12 +0100

    created two endpoints for backend: headlines and filter

--- api/App.js
+++ api/App.js
@@ -6,19 +6,24 @@ const port = 3010
 const token = '7854dfdaa951420e99c303c809f378a0'
 
 /*
-app.get('/', (req, res) => res.send('Hello World!'))
-app uses http methods and when one is used e.g. app.get
-'/' meaning something like localhost:8000/
-or '/news'  meaning localhost:8000/news
-then you have a function with 2 variables, req and res.
-req is the request, so if the user includes a querystring or some POST parameters, they'll be in there.
-res is the response, which is empty.. and your function has to put something into it to send back
-(which is why it then says, res.send, because it's adding something to the response for your client to see)
+  Returns top headlines for the US
 */
+app.get('/headlines', (req, res) => {
+  request(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${token}`,{ json: true }, (err, response, body) => {
+    if (err) { return console.log(err); }
+    res.send(body)
+  });
+});
 
-request('https://newsapi.org/v2/top-headlines?country=us&apiKey=7854dfdaa951420e99c303c809f378a0', { json: true }, (err, res, body) => {
-  if (err) { return console.log(err); }
-  console.log("hello");
+/*
+  Takes a query string argument (q) and returns filtered results 
+*/
+app.get('/filter', (req, res) => {
+  const query = req.query.q
+  request(`https://newsapi.org/v2/everything?q=${query}&apiKey=${token}`, { json: true }, (err, response, body) => {
+    if (err) { return console.log(err); }
+    res.send(body)
+  });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
