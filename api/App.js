cd1c681b749cec6f5e46d1929e6e43dd9b3ca228
Emma P <undocontrol88@gmail.com>
2019-07-29 11:35:27 +0100

    Updates API to allow requests from anywhere

--- api/App.js
+++ api/App.js
@@ -2,14 +2,21 @@ const express = require('express')
 const request = require('request');
 //this is essentially the API 
 const app = express()
-const port = 3010
+const port = 8080
 const token = '7854dfdaa951420e99c303c809f378a0'
 
+app.use(function(req, res, next) {
+  // Allow requests from anywhere
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
+
 /*
-  Returns top headlines for the US
+  Returns top headlines for the UK
 */
 app.get('/headlines', (req, res) => {
-  request(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${token}`,{ json: true }, (err, response, body) => {
+  request(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${token}`, (err, response, body) => {
     if (err) { return console.log(err); }
     res.send(body)
   });
@@ -20,7 +27,7 @@ app.get('/headlines', (req, res) => {
 */
 app.get('/filter', (req, res) => {
   const query = req.query.q
-  request(`https://newsapi.org/v2/everything?q=${query}&apiKey=${token}`, { json: true }, (err, response, body) => {
+  request(`https://newsapi.org/v2/everything?q=${query}&apiKey=${token}`, (err, response, body) => {
     if (err) { return console.log(err); }
     res.send(body)
   });
